6bdeabd08ff99f4bd78d34eb68f5b451093ef3b7
Filippo-Livirizzi <livirizzifilippo@gmail.com>
2024-03-07 22:32:18 +0100

    ADD: #

---
+++ js/app.js
@@ -0,0 +1,8 @@
+// Richiesta al cliente il numero di kilometri che vuole percorre
+const stringkil = prompt("Inserire i kilometri che si vogliono percorre:");//string|null
+const kil = parseInt(stringkil);    // conversione da stringa ad un numero intero 
+console.log(kil);   //string|null
+// Richiesta al cliente la propria età
+const stringeta = prompt("Inserire l'età:");
+const eta = parseInt(stringeta);    // conversione da stringa ad un numero intero 
+console.log(eta);   //string|null
