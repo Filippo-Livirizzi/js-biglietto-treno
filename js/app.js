5538ec1a187f991632afd87557914cebe9e507c7
Filippo-Livirizzi <livirizzifilippo@gmail.com>
2024-03-09 17:32:37 +0100

    ADD: #

--- js/app.js
+++ js/app.js
@@ -1,8 +1,25 @@
-// Richiesta al cliente il numero di kilometri che vuole percorre
-const stringkil = prompt("Inserire i kilometri che si vogliono percorre:");//string|null
-const kil = parseInt(stringkil);    // conversione da stringa ad un numero intero 
-console.log(kil);   //string|null
+// Richiesta al cliente il numero di km che vuole percorre
+const km = parseInt( prompt("Inserire i km che si vogliono percorre:")); //number
+console.log(km);
 // Richiesta al cliente la propria età
-const stringeta = prompt("Inserire l'età:");
-const eta = parseInt(stringeta);    // conversione da stringa ad un numero intero 
-console.log(eta);   //string|null
+const eta = parseInt (prompt("Inserire l'età:") );
+console.log(eta);
+ //prezzo al km
+ const  pricekm = 0.21;
+ //sconto
+ let sconto
+ //prezzo del biglietto in base i km inseriti
+ const subprice = km * pricekm;
+ console.log(subprice);
+
+ //SE eta < di 18 anni verrà applicato uno sconto del 20%
+ if(eta < 18){
+    sconto = subprice * 0.2;
+ }else if (eta > 65){ //ALTRIMENTI SE eta > 65 anni verrà applicato uno sconto del 40%
+    sconto = subprice * 0.4;
+ } else{ //ALTRIMENTI se eta è compreso tra 18 e 65 non verrà applicato nessuno sconto 
+    sconto = 0
+ }
+ //calcolo del prezzo finale
+ const prezzo = subprice - sconto;
+ console.log(prezzo.toFixed(2),prezzo); 
